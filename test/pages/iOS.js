7b17f979cc4f25ba2e0b531ee47ec06ad25b29d9
jkansagor <jkansagor@newrelic.com>
2025-10-03 13:40:51 -0700

    feat: Add page object

---
+++ test/pages/iOS.js
@@ -0,0 +1,170 @@
+class iOS {
+  constructor(driver) {
+    this.driver = driver;
+  }
+
+  // LOCATORS
+  // Universal app locators
+  get home() {
+    return this.driver.$("id:Home");
+  }
+
+  // get background() {
+  //   return this.driver.executeScript("mobile:pressButton", [{ name: "home" }]);
+  // }
+
+  // Todo tab Locators
+  get todoTab() {
+    return this.driver.$("accessibility id:TodoList");
+  }
+
+  get todoTextfield() {
+    return this.driver.$("accessibility id:text-input-flat");
+  }
+  get addButton() {
+    return this.driver.$("accessibility id:Add");
+  }
+
+  get returnButton() {
+    return this.driver.$("accessibility id:Return");
+  }
+
+  // Explore tab locators
+
+  get exploreTab() {
+    return this.driver.$("accessibility id:Explore");
+  }
+
+  get hexButton() {
+    return this.driver.$("accessibility id:HandledException");
+  }
+
+  get goodHTTRequestButton() {
+    return this.driver.$("accessibility id:Good Http Request");
+  }
+  get badHTTPRequestButton() {
+    return this.driver.$("accessibility id:Bad Http Request");
+  }
+
+  get dtRequestButton() {
+    return this.driver.$("accessibility id:Distributed Tracing Request");
+  }
+
+  get delayedRequestButton() {
+    return this.driver.$("accessibility id:Delayed Http Request");
+  }
+
+  // Crash tab locator
+
+  get crashTab() {
+    return this.driver.$("accessibility id:Crash");
+  }
+
+  get uriErrorButton() {
+    return this.driver.$("accessibility id:URI Error");
+  }
+
+  get typeErrorButton() {
+    return this.driver.$("accessibility id:Type Error");
+  }
+
+  get evalErrorButton() {
+    return this.driver.$("accessibility id:Eval Error");
+  }
+
+  // Methods
+
+  async launchApp() {
+    await driver.execute("mobile: launchApp", {
+      bundleId: "com.newrelic.mainagenttestapp",
+    });
+    await driver.setTimeouts(5000);
+  }
+
+  async backgroundApp() {
+    await driver.executeScript("mobile:pressButton", [{ name: "home" }]);
+    await driver.setTimeouts(5000);
+  }
+
+  async clickHomeTab() {
+    await this.home.waitForExist({ timeout: 30000 });
+    await this.home.click();
+  }
+
+  async clickTodoTab() {
+    await this.todoTab.waitForExist({ timeout: 3000 });
+    await this.todoTab.click();
+    await this.driver.setTimeouts(3000);
+  }
+
+  async clickExploreTab() {
+    await this.exploreTab.waitForExist({ timeout: 3000 });
+    await this.exploreTab.click();
+    await this.driver.setTimeouts(3000);
+  }
+
+  async clickCrashTab() {
+    await this.crashTab.waitForExist({ timeout: 3000 });
+    await this.crashTab.click();
+    await this.driver.setTimeouts(3000);
+  }
+
+  async addTodo() {
+    await this.todoTextfield.waitForExist({ timeout: 3000 });
+    await this.todoTextfield.click();
+    await this.driver.setTimeouts(3000);
+
+    const typeH = await this.driver.$("accessibility id:H");
+    await typeH.click();
+    await this.driver.setTimeouts(3000);
+    const typeE = await this.driver.$("accessibility id:e");
+    await typeE.click();
+    await this.driver.setTimeouts(3000);
+    const typeL = await this.driver.$("accessibility id:l");
+    await typeL.click();
+    await this.driver.setTimeouts(3000);
+    await typeL.click();
+    await this.driver.setTimeouts(3000);
+    const typeO = await this.driver.$("accessibility id:o");
+    await typeO.click();
+    await this.driver.setTimeouts(3000);
+
+    await this.addButton.click();
+    await this.driver.setTimeouts(3000);
+    await this.returnButton.click();
+    await this.driver.setTimeouts(3000);
+  }
+
+  // Use me to click on a button on the Explore tab. In this case, `eventName` is the text of the button
+  async waitAndClickExploreEventButton(eventName) {
+    const buttons = {
+      ["hex"]: () => this.hexButton,
+      ["goodHTTP"]: () => this.goodHTTRequestButton,
+      ["badHTTP"]: () => this.badHTTPRequestButton,
+      ["delayedHTTP"]: () => this.delayedRequestButton,
+      ["dtRequest"]: () => this.dtRequestButton,
+    };
+    await buttons[eventName]().waitForExist({ timeout: 3000 });
+    await buttons[eventName]().click();
+    console.log("clicked ", eventName);
+    await this.driver.setTimeouts(3000);
+  }
+
+  // Use me to click on a button on the Crash tab. In this case, `eventName` is the type of crash
+  async forceCrash(eventName) {
+    const buttons = {
+      ["uri"]: () => this.uriErrorButton,
+      ["type"]: () => this.typeErrorButton,
+      ["eval"]: () => this.evalErrorButton,
+    };
+
+    await this.driver.setTimeouts(10000);
+    await buttons[eventName]().waitForExist({ timeout: 3000 });
+    await buttons[eventName]().click();
+    await this.driver.setTimeouts(7000);
+    await this.launchApp();
+    await this.driver.setTimeouts(10000);
+  }
+}
+
+module.exports = iOS;
