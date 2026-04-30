53259353105c3cfbdff36d54b8a4eab77253761c
ndesai <ndesai@newrelic.com>
2026-04-29 22:00:07 -0500

    update script for other mobil events

--- test/android-crash.js
+++ test/android-crash.js
@@ -15,15 +15,39 @@ describe("main agent test app - android", () => {
     const URI = "uri";
     const Type = "type";
 
+     const HEX = "hex"; 
+    const HTTPRequest = "HTTPRequest";
+    const HTTPRequestError = "HTTPRequestError"; 
+    const DT = "dtRequest"; 
+    const DELAYED = "delayedHTTP"; // network failure
+    await androidPage.clickExploreTab();
+
+    await androidPage.waitAndClickExploreEventButton(HEX);
+    await androidPage.waitAndClickExploreEventButton(HEX);
+    await androidPage.waitAndClickExploreEventButton(HTTPRequest);
+    await androidPage.waitAndClickExploreEventButton(DELAYED);
+    await androidPage.waitAndClickExploreEventButton(DT);
+    await androidPage.waitAndClickExploreEventButton(HTTPRequest);
+    await androidPage.waitAndClickExploreEventButton(HTTPRequestError);
+    await androidPage.waitAndClickExploreEventButton(HTTPRequestError);
+    await androidPage.waitAndClickExploreEventButton(HTTPRequestError);
+    await androidPage.waitAndClickExploreEventButton(HEX);
+
     await androidPage.launchApp()
+   await navigateAndClickExploreEvents(androidPage, HEX, HTTPRequest, DELAYED, DT, HTTPRequestError);
     await androidPage.clickCrashTab();
     await androidPage.forceCrash(RANGE);
+   await navigateAndClickExploreEvents(androidPage, HEX, HTTPRequest, DELAYED, DT, HTTPRequestError);
     await androidPage.clickCrashTab();
     await androidPage.forceCrash(REFERENCE);
+       await navigateAndClickExploreEvents(androidPage, HEX, HTTPRequest, DELAYED, DT, HTTPRequestError);
+
     await androidPage.clickCrashTab();
     await androidPage.forceCrash(EVAL);
+    await navigateAndClickExploreEvents(androidPage, HEX, HTTPRequest, DELAYED, DT, HTTPRequestError);
     await androidPage.clickCrashTab();
     await androidPage.forceCrash(URI);
+    await navigateAndClickExploreEvents(androidPage, HEX, HTTPRequest, DELAYED, DT, HTTPRequestError);
     await androidPage.clickCrashTab();
     await androidPage.forceCrash(Type);
   
@@ -31,3 +55,14 @@ describe("main agent test app - android", () => {
     await androidPage.backgroundApp();
   });
 });
+async function navigateAndClickExploreEvents(androidPage, HEX, HTTPRequest, DELAYED, DT, HTTPRequestError) {
+  await androidPage.clickExploreTab();
+  await androidPage.waitAndClickExploreEventButton(HEX);
+  await androidPage.waitAndClickExploreEventButton(HTTPRequest);
+  await androidPage.waitAndClickExploreEventButton(DELAYED);
+  await androidPage.waitAndClickExploreEventButton(DT);
+  await androidPage.waitAndClickExploreEventButton(HTTPRequest);
+  await androidPage.waitAndClickExploreEventButton(HTTPRequestError);
+  await androidPage.waitAndClickExploreEventButton(HTTPRequestError);
+}
+
--- test/ios-crash.js
+++ test/ios-crash.js
@@ -14,14 +14,29 @@ describe("main agent test app - ios", () => {
     const EVAL = "eval";
     const RANGE = "range";
     const REFERENCE = "reference";
-  
+    
+  const HEX = "hex";
+    const HTTPRequest = "HTTPRequest";
+    const HTTPRequestError = "HTTPRequestError";
+    const DT = "dtRequest";
+    const DELAYED = "delayedHTTP";
+
+    await iOSPage.launchApp();
+
+    await navigateAndClickExploreEvents(iOSPage, HEX, HTTPRequest, DT, HTTPRequestError, DELAYED);
 
     await iOSPage.clickCrashTab();
     await iOSPage.forceCrash(URI);
+        await navigateAndClickExploreEvents(iOSPage, HEX, HTTPRequest, DT, HTTPRequestError, DELAYED);
+
     await iOSPage.clickCrashTab();
     await iOSPage.forceCrash(TYPE);
+        await navigateAndClickExploreEvents(iOSPage, HEX, HTTPRequest, DT, HTTPRequestError, DELAYED);
+
     await iOSPage.clickCrashTab();
+
     await iOSPage.forceCrash(EVAL);
+        await navigateAndClickExploreEvents(iOSPage, HEX, HTTPRequest, DT, HTTPRequestError, DELAYED);
     await iOSPage.clickCrashTab();
     await iOSPage.forceCrash(RANGE);
     await iOSPage.clickCrashTab();
@@ -30,3 +45,17 @@ describe("main agent test app - ios", () => {
      await iOSPage.backgroundApp();
   });
 });
+async function navigateAndClickExploreEvents(iOSPage, HEX, HTTPRequest, DT, HTTPRequestError, DELAYED) {
+  await iOSPage.clickExploreTab();
+
+  await iOSPage.waitAndClickExploreEventButton(HEX);
+  await iOSPage.waitAndClickExploreEventButton(HTTPRequest);
+  await iOSPage.waitAndClickExploreEventButton(DT);
+  await iOSPage.waitAndClickExploreEventButton(HTTPRequestError);
+  await iOSPage.waitAndClickExploreEventButton(DELAYED);
+  await iOSPage.waitAndClickExploreEventButton(HTTPRequestError);
+  await iOSPage.waitAndClickExploreEventButton(HTTPRequestError);
+  await iOSPage.waitAndClickExploreEventButton(DELAYED);
+  await iOSPage.waitAndClickExploreEventButton(DELAYED);
+}
+
